9be17e5b53923523ead294338c52139009fe476c
DybalaBart117 <oliverm100702@gmail.com>
2025-01-29 19:52:54 -0600

    Se agregó otro comentario

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
 // Este archivo configura el store de Redux, que es donde se almacena el estado global de la aplicación.
+//Este es el store, claro que si
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../features/usersSlice";
 
